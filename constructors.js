a60a696e602002ebbfa37d50758bff0cf90869ff
Yvonne-Atieno <oyvonneatieno@gmail.com>
2023-05-02 07:40:16 +0300

    first commit

---
+++ constructors.js
@@ -0,0 +1,90 @@
+// Create a Car class or function constructor that has the following properties:
+//a. make (string): The make of the car, e.g., "Toyota".
+//b. model (string): The model of the car, e.g., "Camry".
+//c. year (number): The year the car was manufactured, e.g., 2020.
+//d. isAvailable (boolean): Indicates if the car is currently available for rent.
+//The Car class or function constructor should also have a method called
+//toggleAvailability that changes the isAvailable property to its opposite value (true to
+//false, false to true)
+class car{
+    constructor(make,model,year,isAvailable){
+        this.make=make;
+        this.model=model;
+        this.year=year;
+        this.isAvailiable=this.isAvailiable;`   `
+    }
+    toggleAvailability(){
+        this.isAvailiable=!this.isAvailiable;
+    }
+}
+const car2=new car("toyota","camry",2020);
+console.log(car2)
+
+    
+     
+
+
+
+
+//2) Create a Rental class or function constructor that has the following properties:
+//● car (Car object): The car that has been rented.
+//● renterName (string): The name of the person who rented the car.
+// rentalStartDate (Date object): The start date of the rental period.
+//rentalEndDate(Data objects):The end date of the rental period.
+class Rental{
+    constructor(car,renterName,rentalStartDate,rentalEndDate){
+        this.car=car;
+        this.renterName=renterName;
+        this.rentalStartDate=rentalStartDate;
+        this.rentalEndDate=rentalEndDate
+    }
+    rentalEndDate(){
+        this.thisrentalEndDate=!this.rentalEndDate;
+    }
+}
+const rental1=new rental("car","renterName","rentalStartDate","rentalEndDate")
+console.log(rental1)
+
+//reate an instance of the Car class or function constructor for a car in the
+//inventory. Then, create an instance of the Rental class or function constructor for
+//a rental involving the car you created. Finally, calculate the rental duration using
+//the calculateRentalDuration method.
+class car{
+    constructor(toyota,camry)
+    this.toyota;
+    this.camry;
+class rental{
+    constructor(toyota,camry)
+}
+
+}const car=new car(toyota,cmry)
+console.log(car)
+
+//Create a Rental class or function constructor that has the following properties:
+//● car (Car object): The car that has been rented.
+//● renterName (string): The name of the person who rented the car.
+//● rentalStartDate (Date object): The start date of the rental period.
+//● rentalEndDate (Date object): The end date of the rental period.
+//The Rental class or function constructor should also have a method called
+//calculateRentalDuration that returns the rental duration in days.
+class rental{
+    constructor(car,renterName,rentalStartDate,rentalEndDate){
+        this.car=car;
+        this.renterName=renterName;
+        this.rentalEndDate=rentalEndDate;
+        this,rentalStartDate=rentalStartDate;
+    }
+    calculateRentalDuration(){
+        this.iscalculateRentalDuration=!this.calculateRentalDuration;
+    }
+}
+const rental3= new rental(name,startdate,enddate)
+console.log(rental3)
+
+//3) Create an instance of the Car class or function constructor for a car in the
+//inventory. Then, create an instance of the Rental class or function constructor for
+//a rental involving the car you created. Finally, calculate the rental duration using
+
+
+
+
